361bb1e83767164c69232c3196bf6deb0f3631d3
Diego27SEN <banitessebastian2decundaria@gmail.com>
2025-11-29 13:14:29 -0500

    spiritudesuelo

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,28 @@
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstaclePrefab;//Prefab del obstáculo
+    public Transform[] spawnPoints; //Puntos donde puede aparecer
+
+    public float spawnInterval = 20f;//Cada cuanto aparece el fantasma
+    public float obstacleLifetime = 7f;//Duración del obstáculo
+    private GameObject currentObstacle;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnObstacle), spawnInterval, spawnInterval);
+    }
+
+    void SpawnObstacle()
+    {
+        if (currentObstacle != null) return; // Si ya hay un obstáculo, no crear otro
+        int randomIndex = Random.Range(0, spawnPoints.Length); // Elegir un punto aleatorio
+        Transform spawnPoint = spawnPoints[randomIndex];
+
+        currentObstacle = Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity); // Instanciar el obstáculo
+        Destroy(currentObstacle, obstacleLifetime);// Destruirlo luego del tiempo configurado
+    }
+
+}
